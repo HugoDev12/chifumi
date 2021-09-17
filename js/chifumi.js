394ddab776b62e7f9eae76dbab56007260d8c33c
Occelli <occelli.hugo@hotmail.fr>
2021-09-17 17:20:26 +0200

    commencement de la partie

--- js/chifumi.js
+++ js/chifumi.js
@@ -5,79 +5,55 @@ let welcome = alert("Bienvenue, détendez vous. Vous allez jouer au Chifoumi !")
 
 // asking player's name and firstname only
 
-// let test = 455;
-// console.log(+test);
-
-// let userName = prompt("Veuillez entrer votre nom.");
-// let userFirstname = prompt("Veuillez entrez votre prénom.");
-// if (/^[a-zA-Z]+$/.test(userName) && /^[a-zA-Z]+$/.test(userFirstname)){
-//     console.log("test réussi");
-// } else { console.log("Recommencez");}
 
 let userName = null;
 let userFirstname = null;
+let correct = null;
 let error = null;
+// let proposal = null;
+// let scissors = null;
 
-if (/^[a-zA-Z]+$/.test(userName = prompt("Veuillez entrer votre nom."))
+function promptBox1 (){
+
+    if (/^[a-zA-Z]+$/.test(userName = prompt("Veuillez entrer votre nom."))
     && (/^[a-zA-Z]+$/.test(userFirstname = prompt("Veuillez entrer votre prénom")))){
-    console.log("test réussi");
-} else { error = alert("Recommencez");}
+    correct = alert("Bonjour " + userName + " " + userFirstname + " , vous allez commencer une partie contre l'ordinateur!");
+    } else { error = alert("Recommencez :p");}
+    return {userName, userFirstname}
+};
 
 
 
-// if (/^[a-zA-Z]+$/.test(userName) && /^[a-zA-Z]+$/.test(userFirstname)){
-//     let userFirstname = prompt("Veuillez entrez votre prénom.");
-// } else if (/^[a-zA-Z]+$/.test(userFirstname)) 
-//     {
-//         console.log("test réussi");
-//     } else { let error = alert("Recommencez :)");}
-// else { let error = alert("Recommencez :p"); }
+// prompt box and verify if it contains a string in both statements
+promptBox1();
+
+if (userName != null){promptBox2();}
+
+function promptBox2(){
+    let proposal = prompt("Choisissez entre Pierre,Papier ou Ciseaux");
+    return proposal
+}
 
 
 
-// switch (userName) {
-//     case userName.length[0] = "":
-//         console.log("Veuillez rentrer quelque chose :p");
-//         break;
+// correctEntry();
 
-// }
+// function correctEntry(proposal){
+//     if (proposal ==='Pierre' || 'pierre' 
+//         && proposal === 'Papier' || 'papier'
+//         && proposal === 'Ciseaux' || 'ciseaux'){ 
 
-// if (userName.length === 0 && userName != ""){
-//     console.log("c'est faux");
 //     }
-//     else { console.log("correct"); }
-
-// let userFirstname = prompt("Veuillez entrer votre prénom.");
-
-// let userText = userName + " " + userFirstname;
-
-
-// let userText = prompt("Veuillez entrer votre nom et votre prénom.");
-
-// && userText.length === 0
-// userText != ""
-//  if (userText.length === 0 && userText != "") {
-//     console.log("c'est faux");
-// }
-// else { console.log("correct"); }
-
-//if (userText.length === 0 &
-
-// if (userText != "") {
-//    console.log("faux");
-// }
-// else { console.log("correct"); }
-
-
-// function userName(firstname, lastName){
-//     if (userText != null && +userText === true){
-//         let answer = alert("Bonjour " + userText + ", vous allez commencer la partie.");
-//     }
-//     else {
-//         let elseUserText = prompt("Veuillez entrer votre prénom correctement :p");
-//     }
-
-//     return elseUserText ;
 // }
 
 
+// let rock = null;
+// let paper = null;
+// let scissors = null;
+
+
+
+
+// function proposal(r,p,s){
+
+// }
